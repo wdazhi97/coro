f67721346e91e2e0cb1856d9a67c4c664c8730e4
montyliu <montyliu@tencent.com>
2024-08-22 15:38:58 +0800

    返回result的demo

--- src/awiterall.cpp
+++ src/awiterall.cpp
@@ -4,15 +4,13 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <coroutine>
+#include <random>
+#include <future>
+
 #include "when_all_ready_task.h"
 
 using namespace cppcoro;
-
-#include <iostream>
-#include <coroutine>
-#include <future>
-#include <thread>
-#include <chrono>
 
 struct outer_task
 {
@@ -31,51 +29,98 @@ struct outer_task
     };
 };
 
-class wait_time_task
-{
-public:
-    struct promise_type
-    {
-        std::suspend_always initial_suspend() noexcept {  return {}; }
-        std::suspend_never final_suspend() noexcept{ return {};}
-        wait_time_task get_return_object() noexcept { return {}; };
-        void unhandled_exception() {};
-        void return_void() noexcept {}
-    };
-public:
-    auto operator co_await() const &noexcept
-    {
-        struct awaiter {
-            int sleepTime = 0;
+int getrandint(int min, int max) {
+    std::random_device rd;  //如果可用的话，从一个随机数发生器上获得一个真正的随机数
+    std::mt19937 gen(rd()); //gen是一个使用rd()作种子初始化的标准梅森旋转算法的随机数发生器
+    std::uniform_int_distribution<> distrib(min, max);
+    return distrib(gen);
+}
 
-            bool await_ready() {
-                return false;
-            }
-            void await_suspend(std::coroutine_handle<> coroutine_handle) {
-                sleepTime = rand() % 5 + 5;
-                std::thread([this, coroutine_handle]() {
-                   std::this_thread::sleep_for(std::chrono::seconds(this->sleepTime));
-                   coroutine_handle.resume();
-                }).detach();
-                std::cout << "task resume after " << std::endl;
-            }
-            int await_resume() {
-                std::cout << "task resume after " << sleepTime  << "s" << std::endl;
-                return sleepTime;
-            }
-        };
-        return awaiter{};
+static int global_task_count = 1;
+
+template<typename T>
+class async_awaiter_base {
+public:
+    int sleepTime = 0;
+    int cur_task_count = 0;
+
+    bool await_ready() {
+        return false;
+    }
+
+    void await_suspend(std::coroutine_handle<> coroutine_handle) {
+        sleepTime = getrandint(5, 10);
+        std::thread([this, coroutine_handle]() {
+           std::this_thread::sleep_for(std::chrono::seconds(this->sleepTime));
+           coroutine_handle.resume();
+        }).detach();
+        std::cout << "task " << cur_task_count <<  " suspend " << std::endl;
+    }
+
+    void await_resume() {
+        std::cout << "task " << cur_task_count <<  " resume after " << sleepTime  << "s" << std::endl;
+    }
+
+    async_awaiter_base(){
+        cur_task_count = global_task_count;
+        global_task_count++;
     }
 };
+
+
+template<typename T>
+class async_awaiter : public async_awaiter_base<T> {
+
+};
+
+template<>
+class async_awaiter<int> : public async_awaiter_base<int> {
+public:
+    int await_resume() {
+        std::cout << "task " << cur_task_count <<  " resume after " << sleepTime  << "s" << std::endl;
+        return sleepTime;
+    }
+};
+
+template<>
+class async_awaiter<float> : public async_awaiter_base<float> {
+public:
+    float await_resume() {
+        std::cout << "task " << cur_task_count <<  " resume after " << sleepTime  << "s" << std::endl;
+        return (float) sleepTime;
+    }
+};
+
+template<>
+class async_awaiter<std::string> : public async_awaiter_base<float> {
+public:
+    explicit async_awaiter(std::string&& result) : _result(result) {}
+    std::string _result;
+
+    std::string await_resume() {
+        std::cout << "task " << cur_task_count <<  " resume after " << sleepTime  << "s" << std::endl;
+        return _result;
+    }
+};
+
+template<typename T>
+auto operator co_await(async_awaiter<T> awaiter) {
+    return awaiter;
+}
 
 outer_task outer_tasks()
 {
     std::cout << "outer coro begin ...." << std::endl;
-    //auto task = when_all_ready(wait_time_task_func(), wait_time_task_func());
-    auto [task1, task2] = co_await when_all_ready(wait_time_task(), wait_time_task());
 
-    std::cout << "task1 sleep time " << task1 << "s" << std::endl;
-    std::cout << "task2 sleep time " << task2 << "s" << std::endl;
+    auto [task1, task2, task3] = co_await when_all_ready(
+        async_awaiter<int> (),
+        async_awaiter<float> (),
+        async_awaiter<std::string> ("test await")
+        );
+
+    std::cout << "task1 result " << task1 << std::endl;
+    std::cout << "task2 result " << task2 << std::endl;
+    std::cout << "task3 result " << task3 << std::endl;
     std::cout << "outer coro end ...." << std::endl;
     co_return;
 }
@@ -87,7 +132,5 @@ int main()
     while(true) {
         // do something
     }
-    std::cout << "main end ...." << std::endl;
-    //system("pause");
 }
 
